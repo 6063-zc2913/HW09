5624565e507104e8e9f71a8994bab2d9a0a8c1d9
Ziyi Chen <zc2913@nyu.edu>
2023-11-06 23:48:50 -0500

    Pattern

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let COLS = createCols("https://coolors.co/eb300f-fe7688-fff566-212121-306e42-0d3
 let PALETTE;
 
 function setup() {
-	const s = min(windowWidth, windowHeight) * 0.9;
+	const s = min(windowWidth, windowHeight) * 1;
 	createCanvas(s, s);
 	background(240);
 	frameRate(0.75);
@@ -10,8 +10,8 @@ function setup() {
 
 function draw() {
 	PALETTE = shuffle(COLS, true);
-	background(PALETTE[3]);
-	PALETTE = PALETTE.slice(0, 3);
+	background(PALETTE[5]);
+	PALETTE = PALETTE.slice(1, 4);
 	
 	rectMode(CENTER);
 	
@@ -19,60 +19,61 @@ function draw() {
 	typo(width * 0.49, height /2, width / 4, height /4 / 2 * 7, false);
 	typo(width /2 + width / 3.2, height /2, width / 4, height /4 / 2 * 7, true);
 	
-	const d = width * 0.08;
+	const d = width * 0.25;
 	pattern(randPattern(d));
-	circlePattern(width  * 0.6, height * 0.75, d);
+  rectPattern(180,height/2, d, d);
 }
 
-
-function typo(cx, cy, w, h, isp)
-{
-	const structure = isp ? 
-				[[-9, -9],[-9, -9],[0, 1],[-1, -1], [-1, 2], [-1, -9],[2, -9]] :
-				[[-1, 2],[-1, -9],[3, 1],[-9, -1], [0, 2], [-9, -9],[-9, -9]] ;
-
-	const xNum = structure[0].length;
-	const xSpan = w / xNum;
-	const yNum =  structure.length;
-	const ySpan = h / yNum;
-	
-	rectMode(CENTER);
-	ellipseMode(CENTER);
-	
-	push();
-	translate(cx - w /2, cy - h / 2);
-	
-	for(let yi = 0; yi < yNum; yi++)
-	{
-		for(let xi = 0; xi < xNum; xi++)
-		{
-			const isDraw = structure[yi][xi];
-			if(isDraw >= -1)
-			{
-				const x = xSpan * (xi + 0.5);
-				const y = ySpan * (yi + 0.5);
-				patternColors(shuffle(PALETTE));
-				pattern(randPattern(xSpan));
-				patternAngle(int(random(4)) * PI / 4);
-				push();
-				translate(x, y);
-				if(isDraw >= 0){
-					rotate(isDraw * HALF_PI);
-					const rn = random();
-					if(rn > 0.66)rectPattern(0, 0, xSpan, ySpan, xSpan, 0, 0, 0);
-					else if(rn > 0.33) arcPattern(xSpan / 2, ySpan / 2, xSpan * 2, ySpan * 2, PI, TAU / 4 * 3);
-					else trianglePattern(xSpan / 2, ySpan / 2, -xSpan / 2, ySpan / 2, xSpan / 2, -ySpan / 2);
-				}
-				else
-				{
-					rectPattern(0, 0, xSpan, ySpan);
-				}
-				pop();
-			}
-		}
-	}
-	pop();
-}
+ 
+  
+    function typo(cx, cy, w, h, isp)
+    {
+      const structure = isp ? 
+      [[-1, -1], [-1, -1], [-9, -9], [-9, -9], [-1, -1], [-1, -1]] :
+      [[-1, -1], [-1, -1], [-9,-9], [-9, -9], [-1, -1], [-1, -1]] ;
+    
+      const xNum = structure[0].length;
+      const xSpan = w / xNum;
+      const yNum =  structure.length;
+      const ySpan = h / yNum;
+      
+      rectMode(CENTER);
+      ellipseMode(CENTER);
+      
+      push();
+      translate(cx - w /2, cy - h / 2);
+      
+      for(let yi = 0; yi < yNum; yi++)
+      {
+        for(let xi = 0; xi < xNum; xi++)
+        {
+          const isDraw = structure[yi][xi];
+          if(isDraw >= -1)
+          {
+            const x = xSpan * (xi + 0.5);
+            const y = ySpan * (yi + 0.5);
+            patternColors(shuffle(PALETTE));
+            pattern(randPattern(xSpan));
+            patternAngle(int(random(4)) * PI / 4);
+            push();
+            translate(x, y);
+            if(isDraw >= 0){
+              rotate(isDraw * HALF_PI);
+              const rn = random();
+              if(rn > 0.66)rectPattern(0, 0, xSpan, ySpan, xSpan, 0, 0, 0);
+              else if(rn > 0.33) arcPattern(xSpan / 2, ySpan / 2, xSpan * 2, ySpan * 2, PI, TAU / 4 * 3);
+              else trianglePattern(xSpan / 2, ySpan / 2, -xSpan / 2, ySpan / 2, xSpan / 2, -ySpan / 2);
+            }
+            else
+            {
+              rectPattern(0, 0, xSpan, ySpan);
+            }
+            pop();
+          }
+        }
+      }
+      pop();
+    }
 
 
 function randPattern(t)
